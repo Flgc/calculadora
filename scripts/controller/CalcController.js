638be566d23c9ce6b027b5cdb2116b9ada454697
Fabio Luis <ml@fabiotecnico1.biz>
2020-12-24 19:24:39 -0300

    Implementando Eventos do Teclado (Keyboard) na calculadora

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -10,6 +10,7 @@ class CalcController {
     this._currentDate;
     this.initialize();
     this.initButtonsEvents();
+    this.initKeyboard();
   }
 
   initialize() {
@@ -18,6 +19,46 @@ class CalcController {
     }, 1000);
 
     this.setLastNumberToDisplay();
+  }
+
+  initKeyboard() {
+    document.addEventListener('keyup', (e) => {
+      switch (e.key) {
+        case 'Escape':
+          this.clearAll();
+          break;
+        case 'Backspace':
+          this.clearEntry();
+          break;
+        case '+':
+        case '-':
+        case '/':
+        case '*':
+        case '%':
+          this.addOperation(e.key);
+          break;
+        case 'Enter':
+        case '=':
+          this.calc();
+          break;
+        case '.':
+        case ',':
+          this.addDot();
+          break;
+        case '0':
+        case '1':
+        case '2':
+        case '3':
+        case '4':
+        case '5':
+        case '6':
+        case '7':
+        case '8':
+        case '9':
+          this.addOperation(parseInt(e.key));
+          break;
+      }
+    });
   }
 
   addEventListenerAll(element, events, fn) {
