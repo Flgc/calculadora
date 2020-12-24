3b0a523a69fb3b7d224d098baf92ee334c7006ef
Fabio Luis <ml@fabiotecnico1.biz>
2020-12-24 15:49:58 -0300

    Implementando a saida dos numeros no display da calculando

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -58,9 +58,21 @@ class CalcController {
     let last = this._operation.pop();
     let result = eval(this._operation.join(''));
     this._operation = [result, last];
+
+    this.setLastNumberToDisplay();
   }
 
-  setLastNumberToDisplay() {}
+  setLastNumberToDisplay() {
+    let lastNumber;
+    for (let i = this._operation.length - 1; i >= 0; i--) {
+      if (!this.isOperator(this._operation[i])) {
+        lastNumber = this._operation[i];
+        break;
+      }
+    }
+
+    this.displayCalc = lastNumber;
+  }
 
   addOperation(value) {
     if (isNaN(this.getLastOperation())) {
@@ -73,6 +85,8 @@ class CalcController {
         console.log('another string: ', value);
       } else {
         this.pushOperation(value);
+
+        this.setLastNumberToDisplay();
       }
     } else {
       if (this.isOperator(value)) {
