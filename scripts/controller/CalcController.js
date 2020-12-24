acb301e2a625e7fc2b7638504696c40bfcf9981e
Fabio Luis <ml@fabiotecnico1.biz>
2020-12-24 14:20:11 -0300

    Implementando as operaçoes dos botoes da calculadora

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -34,8 +34,38 @@ class CalcController {
     this.displayCalc = 'Error';
   }
 
+  getLastOperation() {
+    return this._operation[this._operation.length - 1];
+  }
+
+  setLastOperation(value) {
+    this._operation[this._operation.length - 1] = value;
+  }
+
+  isOperator(value) {
+    //Returned true or false
+    return ['+', '-', '*', '%', '/'].indexOf(value) > -1;
+  }
+
   addOperation(value) {
-    this._operation.push(value);
+    console.log('A', isNaN(this.getLastOperation()));
+    if (isNaN(this.getLastOperation())) {
+      // String
+      if (this.isOperator(value)) {
+        // Change operator
+        this._setLastOperation(value);
+      } else if (isNaN(value)) {
+        // another string
+        console.log(value);
+      } else {
+        this._operation.push(value);
+      }
+    } else {
+      // Number converted to string
+      let newValue = this.getLastOperation().toString() + value.toString();
+      this.setLastOperation(parseInt(newValue));
+    }
+
     console.log(this._operation);
   }
 
@@ -48,16 +78,24 @@ class CalcController {
         this.clearEntry();
         break;
       case 'soma':
+        this.addOperation('+');
         break;
       case 'subtracao':
+        this.addOperation('-');
         brake;
       case 'divisao':
+        this.addOperation('/');
         brake;
       case 'multiplicacao':
+        this.addOperation('*');
         break;
       case 'porcento':
+        this.addOperation('%');
         break;
       case 'igual':
+        break;
+      case 'ponto':
+        this.addOperation('.');
         break;
       case '0':
       case '1':
