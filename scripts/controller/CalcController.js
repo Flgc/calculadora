850d1aa97db9dfe57993ad98c33e3fd16a1b8630
Fabio Luis <ml@fabiotecnico1.biz>
2020-12-24 19:47:02 -0300

    Implementando Eventos do Teclado (Keyboard)  Ctrl + C e Ctrl + V na calculadora

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -13,12 +13,34 @@ class CalcController {
     this.initKeyboard();
   }
 
+  copyToClipboard() {
+    let input = document.createElement('input');
+    input.value = this.displayCalc;
+
+    document.body.appendChild(input);
+
+    input.select();
+
+    document.execCommand('Copy');
+
+    input.remove();
+  }
+
+  pasteFromClipboard() {
+    document.addEventListener('paste', (e) => {
+      let text = e.clipboardData.getData('Text');
+
+      this.displayCalc = parseFloat(text);
+    });
+  }
+
   initialize() {
     setInterval(() => {
       this.setDisplayDateTime();
     }, 1000);
 
     this.setLastNumberToDisplay();
+    this.pasteFromClipboard();
   }
 
   initKeyboard() {
@@ -56,6 +78,9 @@ class CalcController {
         case '8':
         case '9':
           this.addOperation(parseInt(e.key));
+          break;
+        case 'c':
+          if (e.ctrlKey) this.copyToClipboard();
           break;
       }
     });
