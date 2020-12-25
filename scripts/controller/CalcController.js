7396b4e5bc20fd90959582f5b6ec64f90626f0ed
Fabio Luis <ml@fabiotecnico1.biz>
2020-12-25 13:18:47 -0300

    Definindo o valor máximo apresentado no display da calculadora

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -345,6 +345,10 @@ class CalcController {
   }
 
   set displayCalc(value) {
+    if (value.toString().length > 10) {
+      this.setError();
+      return false;
+    }
     this._displayCalcE1.innerHTML = value;
   }
 
