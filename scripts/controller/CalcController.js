fbb1f39e855b1a534fe6948eabed979d640c4d3a
Fabio Luis <ml@fabiotecnico1.biz>
2020-12-25 13:00:29 -0300

    Implementando audio através do botão AC na calculadora

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -1,7 +1,10 @@
 class CalcController {
   constructor() {
+    this._audio = new Audio('click.mp3');
+    this._audioOnOff = false;
     this._lastOperator = '';
     this._lastNumber = '';
+
     this._operation = [];
     this._locale = 'pt-BR';
     this._displayCalcE1 = document.querySelector('#display');
@@ -41,10 +44,29 @@ class CalcController {
 
     this.setLastNumberToDisplay();
     this.pasteFromClipboard();
+
+    document.querySelectorAll('.btn-ac').forEach((btn) => {
+      btn.addEventListener('dblclick', (e) => {
+        this.toggleAudio();
+      });
+    });
+  }
+
+  toggleAudio() {
+    this._audioOnOff = !this._audioOnOff;
+  }
+
+  playAudio() {
+    if (this._audioOnOff) {
+      this._audio.currentTime = 0;
+      this._audio.play();
+    }
   }
 
   initKeyboard() {
     document.addEventListener('keyup', (e) => {
+      this.playAudio();
+
       switch (e.key) {
         case 'Escape':
           this.clearAll();
@@ -229,6 +251,8 @@ class CalcController {
   }
 
   execBtn(value) {
+    this.playAudio();
+
     switch (value) {
       case 'ac':
         this.clearAll();
